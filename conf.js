f6ede8dceb7386ec428e3690923c3fe98da72275
Refael Ackermann <refael@empeeric.com>
2014-06-14 22:25:37 +0300

    Initial

---
+++ conf.js
@@ -0,0 +1,31 @@
+"use strict";
+var _ = require('lodash-contrib');
+
+function createConfiguration(appName) {
+    var conf = _.defaults(process.env, {
+        APP_NAME: appName,
+        PORT: process.env.PORT || 80,
+        MONGODB_URI: 'mongodb://localhost/' + appName,
+        IMPORTER_SECRET: '2fzhUceNPjTcFKjE',
+        PASSPORT_COOKIE_NAME: 'auth-' + appName,
+        COOKIE_SECRET: 'Uk5ZWthLqzUk5ZWthLqzUk5ZWthLqz',
+        SENDGRID_AUTH: {
+            user: process.env.SENDGRID_USERNAME || 'app20278627@heroku.com',
+            pass: process.env.SENDGRID_PASSWORD || 's65jcndz'
+        },
+        SOCKET_TRANSPORTS: "websocket htmlfile xhr-polling jsonp-polling".split(" "),
+        FAKE_SOCKET: {
+            emit: function () {},
+            on: function () {},
+            isFake: true
+        },
+        userSockets: {}
+    });
+    conf.MONGODB_URI = conf.MONGOLAB_URI || conf.MONGOHQ_URL || conf.MONGODB_URI;
+    conf.createConfiguration = createConfiguration;
+
+    return module.exports = global.conf = conf;
+}
+
+
+createConfiguration(process.env.APP_NAME || 'boilerplate');
---
+++ index.js
@@ -0,0 +1,25 @@
+"use strict";
+process.chdir(__dirname);
+
+var opinion = require('opinion');
+var socket = require('socket.io');
+var conf = require('./conf');
+
+
+var app = opinion({
+    middlewareOrder: opinion.DEFAULT_MIDDLEWARE_STACK,
+    keys: ['78fd9fe83f2af46f2a8b567154db8d2a'],
+    statics: 'assets',
+    render: ['views', 'dust']
+});
+
+app.use(
+    function* () {
+        this.body = yield this.render('hello-world');
+    }
+);
+
+
+app.listen(conf.PORT, function () {
+    console.log("Server listening on %s", this._connectionKey);
+});
